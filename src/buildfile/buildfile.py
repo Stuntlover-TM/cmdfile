b861b13cd27db7ca36c8728f8609469f087c62ae
Stuntlover <101748104+Stuntlover-TM@users.noreply.github.com>
2023-03-11 14:01:29 +0100

    Add variables to buildfile

--- src/buildfile/buildfile.py
+++ src/buildfile/buildfile.py
@@ -1,6 +1,10 @@
 import os
 
+variables = {}
+
 def _load(filename="build"):
+    global variables
+
     with open(filename, "r") as buildfile:
         contents = buildfile.read().splitlines()
         commands = {}
@@ -14,15 +18,34 @@ def _load(filename="build"):
                 callname = line.replace("(", "").replace(")", "")
                 commands[callname] = []
                 continue
+
             try:
-                commands[callname].append(line)
+                var = line.split(" = ")
+                key = var[0]
+                value = var[1]
+                variables[key] = value
+                is_variable = True
+            except:
+                is_variable = False
+
+            try:
+                commands[callname].append(line) if not is_variable else None
             except UnboundLocalError:
                 raise UnboundLocalError("No table to run")
     
-    return commands
+    return commands, variables
 
 def run(table, filename="build"):
-    tables = _load(filename=filename)
+    tables, variables = _load(filename=filename)
     
     for cmd in tables[table]:
+        if "{_" and "_}" in cmd:
+            varcmd = variables[cmd.split("{_")[1].split("_}")[0]]
+            rest = cmd.split("_}")[1]
+            cmd = cmd.split("{_")[0] + varcmd + rest
         os.system(cmd)
+
+
+def add_var(key, value):
+    global variables
+    variables[key] = value
